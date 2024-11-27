8241edbba24dd5c782e4767a19b8fb38b1a45f42
cxreiff <cooper@cxreiff.com>
2024-11-26 18:19:42 -0800

    added clap argument interface

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,30 @@
 use bevy::prelude::*;
+use clap::{Parser, Subcommand};
 
 use fictui::play;
 
+#[derive(Parser)]
+struct Args {
+    #[command(subcommand)]
+    command: Option<Commands>,
+}
+
+#[derive(Subcommand)]
+#[command(disable_help_subcommand(true))]
+enum Commands {
+    Play,
+    Create,
+    Host,
+}
+
 fn main() {
-    App::new().add_plugins(play::app_plugin).run();
+    let Args { command } = Args::parse();
+
+    match command {
+        Some(Commands::Play) | None => {
+            App::new().add_plugins(play::app_plugin).run();
+        }
+        Some(Commands::Create) => {}
+        Some(Commands::Host) => {}
+    }
 }
